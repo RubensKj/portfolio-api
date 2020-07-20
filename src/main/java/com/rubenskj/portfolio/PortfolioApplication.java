545d69a80d23978afda66b37626f8df3f9a3a8d3
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-07-20 20:36:58 -0300

    Adding environment

--- src/main/java/com/rubenskj/portfolio/PortfolioApplication.java
+++ src/main/java/com/rubenskj/portfolio/PortfolioApplication.java
@@ -1,5 +1,6 @@
 package com.rubenskj.portfolio;
 
+import com.kstruct.gethostname4j.Hostname;
 import com.rubenskj.portfolio.configuration.ApplicationConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -7,38 +8,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.EnvironmentAware;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class PortfolioApplication extends SpringBootServletInitializer implements EnvironmentAware {
 
-	private static Class<PortfolioApplication> applicationClass = PortfolioApplication.class;
+    private static Class<PortfolioApplication> applicationClass = PortfolioApplication.class;
 
-	@Value("${spring.profiles.active}")
-	private String activeProfile;
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 
-	@Override
-	public void setEnvironment(Environment environment) {
-		if (this.validateProfile()) {
-			ApplicationConfiguration.setProductionEnvironment();
-		}
-	}
+    @Override
+    public void setEnvironment(Environment environment) {
+        if (this.validateProfile()) {
+            ApplicationConfiguration.setProductionEnvironment();
+        }
 
-	private boolean validateProfile() {
-		return this.activeProfile != null && this.validateProduction();
-	}
+        Properties props = new Properties();
 
-	private boolean validateProduction() {
-		return this.activeProfile.equalsIgnoreCase("PRODUCTION");
-	}
+        props.setProperty("env.computer", Hostname.getHostname());
+        PropertiesPropertySource propertySource = new PropertiesPropertySource("ConputerConfig", props);
+
+        if (environment instanceof StandardEnvironment) {
+            ((StandardEnvironment) environment).getPropertySources().addFirst(propertySource);
+        }
+    }
+
+    private boolean validateProfile() {
+        return this.activeProfile != null && this.validateProduction();
+    }
+
+    private boolean validateProduction() {
+        return this.activeProfile.equalsIgnoreCase("PRODUCTION");
+    }
 }

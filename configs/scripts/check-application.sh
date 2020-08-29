d7091ea4fef3240ca943fc91d6bd29ce8c0e8b81
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-08-29 17:30:22 -0300

    Setting up CD

--- configs/scripts/check-application.sh
+++ configs/scripts/check-application.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 default_folder="/opt/$(SERVICE_NAME)"
 
-
 if ! [ -s "$default_folder/application.yml" ]
 then 
     echo "File application.yml does not exists. Moving from artifact folder."
-    mv ./$(ARTIFACT_NAME)/application.yml $default_folder/.
+    mv ./$(ARTIFACT_NAME)/$(SERVICE_NAME)/application.yml $default_folder/.
 fi

626564d4125d7ef5b841d7e87d9c67666bfb2323
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-08-29 17:16:33 -0300

    Adding configs files

---
+++ configs/check-application.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+default_folder="/opt/$(SERVICE_NAME)"
+
+
+if ! [ -s "$default_folder/application.yml" ]
+then 
+    echo "File application.yml does not exists. Moving from artifact folder."
+    mv ./$(ARTIFACT_NAME)/application.yml $default_folder/.
+fi
---
+++ configs/check-environment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+service="/etc/systemd/system/$(SERVICE_NAME).service"
+default_folder="/opt/"
+
+echo "Validating if service exists in the system. Service: $service"
+if ! [ -f "$service" ]
+then
+    echo "$(SERVICE_NAME) does not exists. Moving service from artifact folder."
+    mv ./$(ARTIFACT_NAME)/$(SERVICE_NAME).service /etc/systemd/system/.
+fi
+
+echo "Validating default folder of $default_folder/$(SERVICE_NAME)"
+if ! [ -d "$default_folder/$(SERVICE_NAME)" ]
+then
+    echo "$default_folder/$(SERVICE_NAME) does not exists. Creating the default folder"
+    mkdir $default_folder/$(SERVICE_NAME)
+fi

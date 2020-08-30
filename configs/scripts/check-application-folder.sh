d0c984dd67ffd5a5865b2672a995fc2100471bf8
RubensKj <rubenskleinschmidtjr@gmail.com>
2020-08-30 11:22:29 -0300

    Changing check-application file

---
+++ configs/scripts/check-application-folder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+default_folder="/opt/$(SERVICE_NAME)"
+
+echo "Validating if application.yml in the environment."
+if ! [ -s "$default_folder/application.yml" ]
+then 
+    echo "File application.yml does not exists. Moving from artifact folder."
+    mv ./$(ARTIFACT_NAME)/$(ARTIFACT_NAME)/$(SERVICE_NAME)/application.yml $default_folder/.
+fi
+
+echo "Validating if start.sh exists."
+if ! [ -s "$default_folder/start.sh" ]
+then
+    echo "File start.sh was not found. Moving from artifact folder."
+    mv ./$(ARTIFACT_NAME)/$(ARTIFACT_NAME)/$(SERVICE_NAME)/start.sh $default_folder/.
+fi
--- configs/scripts/check-application.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-default_folder="/opt/$(SERVICE_NAME)"
-
-if ! [ -s "$default_folder/application.yml" ]
-then 
-    echo "File application.yml does not exists. Moving from artifact folder."
-    mv ./$(ARTIFACT_NAME)/$(ARTIFACT_NAME)/$(SERVICE_NAME)/application.yml $default_folder/.
-fi

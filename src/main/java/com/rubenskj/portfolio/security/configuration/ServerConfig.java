58ef4e1d2a6afaa437c38b72e03dad82987092bd
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-09-12 11:55:39 -0300

    Changing server config

--- src/main/java/com/rubenskj/portfolio/security/configuration/ServerConfig.java
+++ src/main/java/com/rubenskj/portfolio/security/configuration/ServerConfig.java
@@ -2,11 +2,16 @@ package com.rubenskj.portfolio.security.configuration;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ServerConfig {
 
@@ -29,10 +34,24 @@ public class ServerConfig {
 
     private Connector getHttpConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        try {
+            File keystore = new ClassPathResource("keyStore.p12").getFile();
+            File truststore = new ClassPathResource("keyStore.p12").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(8443);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass("password");
+            protocol.setTruststoreFile(truststore.getAbsolutePath());
+            protocol.setTruststorePass("password");
+            protocol.setKeyAlias("keyStore");
+
+            return connector;
+        } catch (IOException ex) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", ex);
+        }
     }
 }

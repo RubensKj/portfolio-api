a3364d6769420d7e16862d16752c1edb42be1457
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-09-30 22:56:02 -0300

    Update HelloController.java

--- src/main/java/com/rubenskj/portfolio/controller/HelloController.java
+++ src/main/java/com/rubenskj/portfolio/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping
     public String showCICDWorking() {
-        return "Controller to show CICD working.. POC";       
+        return "Hello there, this is the RubensKj API";       
     }
 }

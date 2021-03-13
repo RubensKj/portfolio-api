59e28433dc7d40a12b71b463fbab9a8b5ae82c39
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-13 15:21:36 -0300

    Adding cors to properties

--- src/main/java/com/rubenskj/portfolio/CorsConfiguration.java
+++ src/main/java/com/rubenskj/portfolio/CorsConfiguration.java
@@ -1,24 +1,33 @@
 package com.rubenskj.portfolio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${com.rubens.cors}")
+    public List<String> cors = new ArrayList<>();
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://localhost:3000",
-                        "http://rubenskj.com",
-                        "https://rubenskj.com"
-                )
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        CorsRegistration corsRegistration = registry
+                .addMapping("/**");
+
+
+        corsRegistration.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+
+
+        if (!cors.isEmpty()) {
+            corsRegistration.allowedOrigins(cors.toArray(new String[0]));
+        }
     }
 }

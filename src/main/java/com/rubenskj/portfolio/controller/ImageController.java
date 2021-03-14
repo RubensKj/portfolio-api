5cc386a6361909ac88f90476292fbbdbe3498034
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-13 21:53:24 -0300

    Changing produces from image controller in order to have a cache of image, to load be even quickly

--- src/main/java/com/rubenskj/portfolio/controller/ImageController.java
+++ src/main/java/com/rubenskj/portfolio/controller/ImageController.java
@@ -2,9 +2,12 @@ package com.rubenskj.portfolio.controller;
 
 import com.rubenskj.portfolio.file.service.ImageService;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -16,13 +19,11 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @GetMapping("/images/{typePath}/{filename:.+}")
+    @GetMapping(value = "/images/{typePath}/{filename:.+}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE})
     @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable("typePath") String typePath, @PathVariable("filename") String filename) {
-
+    public ResponseEntity<Resource> serveFile(@PathVariable("typePath") String typePath, @PathVariable("filename") String filename) throws IOException {
         Resource file = imageService.loadFileAsResource(filename, typePath);
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
-                .body(file);
+
+        return ResponseEntity.status(HttpStatus.OK).body(file);
     }
 }
